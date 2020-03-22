d2f88dd0fa179a81168427c5de84062905c7192b
Marek Ognicki <marek@sigrun.eu>
2020-03-22 17:58:01 +0100

    Added uuid config test:

--- tests/Unit/_003_Scopes/Test011_DirectoryTest.php
+++ tests/Unit/_003_Scopes/Test011_DirectoryTest.php
@@ -296,4 +296,46 @@ class Test011_DirectoryTest extends TestCase
 
         $this->assertSame(['two.php', 'three.php'], $result);
     }
+
+    /**
+     * @covers \Vegfund\Jotta\Client\Scopes\DirectoryScope::list
+     * @covers \Vegfund\Jotta\Client\Scopes\DirectoryScope::uuid
+     * @covers \Vegfund\Jotta\Client\Scopes\DirectoryScope::applyFilters
+     * @throws JottaException
+     */
+    public function test015_list_with_uuid()
+    {
+        $uuids = [];
+        for($i = 0; $i < 4; $i++) {
+            $uuids[] = Uuid::uuid4()->toString();
+        }
+
+        $body = (new ResponseBodyMock())->mountPoint([
+            'name'    => Jotta::MOUNT_POINT_SHARED,
+            'files' => [
+                [
+                    'name'    => 'one.txt',
+                    'uuid' => $uuids[0]
+                ],
+                [
+                    'name'    => 'two.txt',
+                    'uuid' => $uuids[1]
+                ],
+                [
+                    'name'    => 'three.txt',
+                    'uuid' => $uuids[2]
+                ],
+                [
+                    'name'    => 'one.txt',
+                    'uuid' => $uuids[3]
+                ],
+            ],
+        ]);
+
+        $mock = new JottaApiV1Mock($body);
+        $jotta = new JottaClient('a', 'b', $mock->getMock());
+        $result = $jotta->mountPoint()->setMountPoint(Jotta::MOUNT_POINT_SHARED)->uuid($uuids[2])->list();
+
+        $this->assertSame(['three.txt'], $result);
+    }
 }
